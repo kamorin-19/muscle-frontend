7f8bcf89e3ef199ebadfe9182d3dac5b76f663f9
kamorin-19 <159443759+kamorin-19@users.noreply.github.com>
2024-08-29 22:21:37 +0900

    feat:Web APIとのとりあえずのつなぎこみ

--- muscle-frontend/src/app/body-part-master/page.tsx
+++ muscle-frontend/src/app/body-part-master/page.tsx
@@ -1,3 +1,59 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from '@chakra-ui/react';
+
+// BodyPart型の定義
+interface BodyPart {
+  id: number;
+  name: string;
+}
+
 export default function BodyPartMasterPage() {
-    return <h1>部位マスタのコンテンツ</h1>;
-  }
+
+  // useStateでdataを管理
+  const [data, setData] = useState<BodyPart[]>([]);
+
+  useEffect(() => {
+    // データを取得する関数
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://localhost:7253/BodyPart/GetBodyParts');
+        const result = await response.json();
+
+        // APIレスポンスからidとnameを抽出してセット
+        const formattedData = result.map((item: { bodyPartId: number; name: string }) => ({
+          id: item.bodyPartId,
+          name: item.name,
+        }));
+
+        setData(formattedData); // useStateのセッター関数を使用してdataを更新
+      } catch (error) {
+        console.error('データの取得に失敗しました', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>ID</Th>
+            <Th>Name</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.map((item) => (
+            <Tr key={item.id}>
+              <Td>{item.id}</Td>
+              <Td>{item.name}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+}
--- muscle-frontend/src/app/page.tsx
+++ muscle-frontend/src/app/page.tsx
@@ -5,7 +5,6 @@ import { Header } from './components/Header';
 export default function Home() {
   return (
     <ChakraProvider>
-      <Header>Home</Header>
       <Box mt={4} p={4}>
         <p>メインページのコンテンツ</p>
       </Box>
