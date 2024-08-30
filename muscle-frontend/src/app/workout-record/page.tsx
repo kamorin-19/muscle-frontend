002450325fd2c6632331d885ec3b085d02ed9c25
kamorin-19 <159443759+kamorin-19@users.noreply.github.com>
2024-08-30 22:24:48 +0900

    feat:進捗記録のモックの追加

--- muscle-frontend/src/app/workout-record/page.tsx
+++ muscle-frontend/src/app/workout-record/page.tsx
@@ -1,3 +1,98 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from '@chakra-ui/react';
+
+// WorkoutRecord型の定義
+interface DailyRecord {
+  id: number;
+  enforcementDay: Date;
+  exerciseName: string;
+  firstSetCount: number;
+  secondSetCount: number;
+  thirdSetCount: number;
+}
+
 export default function WorkoutRecordPage() {
-    return <h1>筋トレ記録のコンテンツ</h1>;
-  }
+
+  // useStateでdataを管理
+  const [data, setData] = useState<DailyRecord[]>([]);
+
+  useEffect(() => {
+    // データを取得する関数
+    const fetchData = async () => {
+      try {
+        //const response = await fetch('https://localhost:7253/BodyPart/GetBodyParts');
+        //const result = await response.json();
+
+        // APIレスポンスからidとnameを抽出してセット
+        // const formattedData = result.map((item: { bodyPartId: number; name: string }) => ({
+        //   id: item.bodyPartId,
+        //   name: item.name,
+        // }));
+
+        const formattedData = [
+          {
+            id: 1,
+            enforcementDay: new Date('2024-08-30'),
+            exerciseName: 'バーベルスクワット',
+            firstSetCount: 10,
+            secondSetCount: 11,
+            thirdSetCount: 12,
+          },
+          {
+            id: 2,
+            enforcementDay: new Date('2024-08-30'),
+            exerciseName: 'ブルガリアンスクワット',
+            firstSetCount: 10,
+            secondSetCount: 11,
+            thirdSetCount: 12,
+          },
+          {
+            id: 3,
+            enforcementDay: new Date('2024-08-30'),
+            exerciseName: 'ダンベルプレス',
+            firstSetCount: 10,
+            secondSetCount: 11,
+            thirdSetCount: 12,
+          },
+        ];
+
+        setData(formattedData); // useStateのセッター関数を使用してdataを更新
+      } catch (error) {
+        console.error('データの取得に失敗しました', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>id</Th>
+            <Th>enforcementDay</Th>
+            <Th>exerciseName</Th>
+            <Th>firstSetCount</Th>
+            <Th>secondSetCount</Th>
+            <Th>thirdSetCount</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.map((item) => (
+            <Tr key={item.id}>
+              <Td>{item.id}</Td>
+              <Td>{item.enforcementDay.toDateString()}</Td>
+              <Td>{item.exerciseName}</Td>
+              <Td>{item.firstSetCount}</Td>
+              <Td>{item.secondSetCount}</Td>
+              <Td>{item.thirdSetCount}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+}
