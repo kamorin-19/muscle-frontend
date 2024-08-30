b1e2b4c64f6293d35fa0a52f78c6bf5ed5bece85
kamorin-19 <159443759+kamorin-19@users.noreply.github.com>
2024-08-30 22:36:03 +0900

    feat:体重記録のモック作成

--- muscle-frontend/src/app/weight-record/page.tsx
+++ muscle-frontend/src/app/weight-record/page.tsx
@@ -1,3 +1,79 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from '@chakra-ui/react';
+
+// DailyWeight型の定義
+interface DailyWeight {
+  id: number;
+  recordedDay: Date;
+  weight: number;
+}
+
 export default function WeightRecordPage() {
-    return <h1>体重記録のコンテンツ</h1>;
-  }
+  // useStateでdataを管理
+  const [data, setData] = useState<DailyWeight[]>([]);
+
+  useEffect(() => {
+    // データを取得する関数
+    const fetchData = async () => {
+      try {
+        //const response = await fetch('https://localhost:7253/BodyPart/GetBodyParts');
+        //const result = await response.json();
+
+        // APIレスポンスからidとnameを抽出してセット
+        // const formattedData = result.map((item: { bodyPartId: number; name: string }) => ({
+        //   id: item.bodyPartId,
+        //   name: item.name,
+        // }));
+
+        const formattedData = [
+          {
+            id: 1,
+            recordedDay: new Date('2024-08-30'),
+            weight: 81.6,
+          },
+          {
+            id: 2,
+            recordedDay: new Date('2024-08-29'),
+            weight: 81.4,
+          },
+          {
+            id: 3,
+            recordedDay: new Date('2024-08-29'),
+            weight: 81.2,
+          },
+        ];
+
+        setData(formattedData); // useStateのセッター関数を使用してdataを更新
+      } catch (error) {
+        console.error('データの取得に失敗しました', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>id</Th>
+            <Th>recordedDay</Th>
+            <Th>weight</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.map((item) => (
+            <Tr key={item.id}>
+              <Td>{item.id}</Td>
+              <Td>{item.recordedDay.toDateString()}</Td>
+              <Td>{item.weight}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+}
