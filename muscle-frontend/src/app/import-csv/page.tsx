826b5650a0a69e95da917c494ffe051e05586b93
kamorin-19 <159443759+kamorin-19@users.noreply.github.com>
2024-09-15 14:26:55 +0900

    feat:取込画面の画面作成

--- muscle-frontend/src/app/import-csv/page.tsx
+++ muscle-frontend/src/app/import-csv/page.tsx
@@ -1,11 +1,52 @@
 "use client";
 
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  Input,
+  Select
+} from '@chakra-ui/react';
+
 export default function ImportCsvPage() {
   return (
     <>
-      <div>
-        <p>追加ページ</p>
-      </div>
+      <TableContainer>
+        <Button colorScheme="teal" onClick={() => console.log(1)} style={{ cursor: 'pointer' }}>
+          取込
+        </Button>
+        <Button ml={4} colorScheme="teal" onClick={() => console.log(2)} style={{ cursor: 'pointer' }}>
+          テンプレートダウンロード
+        </Button>
+        <br />
+        <Table mt={4} variant="simple">
+          <Thead>
+            <Tr>
+              <Th>EnforcementDay</Th>
+              <Th>ExercisePId</Th>
+              <Th>FirstSetCount</Th>
+              <Th>SecondSetCount</Th>
+              <Th>ThirdSetCount</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+
+          </Tbody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
