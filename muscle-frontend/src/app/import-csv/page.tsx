49b17fdfc41b6b5b606e4656b08d50249cd991a8
kamorin-19 <159443759+kamorin-19@users.noreply.github.com>
2024-09-15 14:43:53 +0900

    feat:取込画面のCSVテンプレートダウンロード機能を追加

--- muscle-frontend/src/app/import-csv/page.tsx
+++ muscle-frontend/src/app/import-csv/page.tsx
@@ -22,13 +22,37 @@ import {
 } from '@chakra-ui/react';
 
 export default function ImportCsvPage() {
+
+  // CSVのテンプレートをダウンロードする
+  const handleDowdloadTemplate = () => {
+
+    // CSVのヘッダー
+    const header = `実施日,種目,1回目,2回目,3回目\n`;
+
+    // Blobを使ってCSVファイルの生成
+    const blob = new Blob([header], { type: 'text/csv;charset=utf-8;' });
+
+    // ダウンロード用のリンクを作成
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'template.csv');
+
+    // リンクをクリックしてCSVをダウンロード
+    document.body.appendChild(link);
+    link.click();
+
+    // リンクを削除
+    document.body.removeChild(link);
+  }
+
   return (
     <>
       <TableContainer>
         <Button colorScheme="teal" onClick={() => console.log(1)} style={{ cursor: 'pointer' }}>
           取込
         </Button>
-        <Button ml={4} colorScheme="teal" onClick={() => console.log(2)} style={{ cursor: 'pointer' }}>
+        <Button ml={4} colorScheme="teal" onClick={() => handleDowdloadTemplate() } style={{ cursor: 'pointer' }}>
           テンプレートダウンロード
         </Button>
         <br />
